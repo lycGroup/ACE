947d1acb4cc24a5590d6f3e8fc4929f2eae15013
sai.qiao <sai.qiao@grandartisans.com>
2018-11-28 15:21:19 +0800

    first commit

---
+++ ace-security/ace-control/ace-monitor/src/main/java/com/github/Joseph/control/monitor/MonitorBootstrap.java
@@ -0,0 +1,21 @@
+package com.github.Joseph.control.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+/**
+ * ClassName: MonitorBootstrap
+ * Description: TODO
+ * Author: Joseph
+ * Date: 2018/11/27 18:03
+ */
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class MonitorBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorBootstrap.class, args);
+    }
+
+}
